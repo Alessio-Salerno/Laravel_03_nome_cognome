55d2169fd526582265c6d91c00e6b86252db5c59
Alessio Salerno <ale.salerno.00@gmail.com>
2024-09-04 09:22:01 +0200

    oggi pusho

--- resources/views/doveSiamo.blade.php
+++ resources/views/doveSiamo.blade.php
@@ -5,12 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-
-    <!-- NAV -->
+    
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
@@ -50,9 +49,8 @@
             </div>
         </div>
     </nav>
-    <!-- FINE NAV -->
+   
 
-    <!-- Contenuto Principale -->
     <div class="container-fluid bg-body-secondary vh-100">
         <div class="row h-75 justify-content-center align-items-center">
             <div class="col-12">
@@ -63,11 +61,6 @@
         </div>
     </div>
 
-
-   
-
-
-    <!-- Sezione Dati -->
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -82,15 +75,6 @@
             </div>
         </div>
     </div>
-    
-
-
-
- 
-
-
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
